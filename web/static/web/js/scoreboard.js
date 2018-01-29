5af9b312093d826349d92e650efd257675e486a9
Odin Jenseg <odin.jenseg@stud.ntnu.no>
2018-01-29 19:35:49 +0100

    Scoreboard lead every fifth minute and remove bold text from scoreboard items

--- web/static/web/js/scoreboard.js
+++ web/static/web/js/scoreboard.js
@@ -1,14 +1,18 @@
-getJSON('/api/scoreboard/', 
-function(err, data) {
-  var scoreboard = $("#scoreboard");
-  for (i = data.length -1; i >= 0; i--) {
-    entry = `
-      <tr>
-        <th scope="row">${i+1}</th>
-        <th><a href="/team/${data[i].team_id}">${data[i].team_name}</a></th>
-        <th>${data[i].team_score}</th>
-      </tr>
-    `;
-    scoreboard.prepend(entry);
-  }
-});
+function get_scores() {
+  getJSON('/api/scoreboard/', function(err, data) {
+    var scoreboard = $("#scoreboard");
+    scoreboard.empty();
+    for (i = data.length -1; i >= 0; i--) {
+      entry = `
+        <tr>
+          <td scope="row">${i+1}</td>
+          <td><a href="/team/${data[i].team_id}">${data[i].team_name}</a></td>
+          <td>${data[i].team_score}</td>
+        </tr>
+      `;
+      scoreboard.prepend(entry);
+    }
+  });
+  setTimeout(get_scores, 5000);
+}
+get_scores();
