f49742ba22a84cecd56c045dc5bdbd0be2a1d422
Odin Jenseg <odin.jenseg@stud.ntnu.no>
2018-02-21 10:26:21 +0100

    Divde settings into base and development

--- yactff/settings/base.py
+++ yactff/settings/base.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(BASE_DIR)
 PAGE_DIR ="{}/web/pages/web".format(BASE_DIR)
 LOGIN_URL = "/login"
 LOGGING_CONFIG = None
@@ -50,13 +51,6 @@ logging.config.dictConfig(LOGGING)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '87k_d^98rd=b#h+c#%&+&+rjgq(+vmp31o12h67)1k@_#=f9ow'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-INTERNAL_IPS = ["127.0.0.1"]
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.auth',
@@ -65,7 +59,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web.apps.WebConfig',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +69,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yactff.urls'
@@ -101,18 +93,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'yactff.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -130,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR + "/media/"
 LOGIN_REDIRECT_URL = 'index'
---
+++ yactff/settings/development.py
@@ -0,0 +1,25 @@
+from yactff.settings.base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+INTERNAL_IPS = ["127.0.0.1"]
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
