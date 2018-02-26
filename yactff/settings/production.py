abd697b58d1fd5a8881fb6faea9da8134bd0fbdd
Odin Jenseg <odin.jenseg@stud.ntnu.no>
2018-02-26 13:12:12 +0100

    Moving from base to production setting and adding databse config

--- yactff/settings/production.py
+++ yactff/settings/production.py
@@ -12,14 +12,21 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
 BROKER_BACKEND = 'memory'
 CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+CTF_NAME = "Capture Tha Flag"
+CTF_START = "2018-02-24 13:00:00"
+CTF_END = "2018-02-24 14:10:00"
+MAX_TEAM_SIZE = 5
